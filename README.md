b5d450902e2bd8c97a1212888d32836fe5e7f7aa
Denny Mueller <denny@dennymueller.de>
2021-11-19 21:20:17 +0100

    add license and some depencies

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Keex
 
+
+
+
 A wrapper for Keen's API. Check the [docs](xxxx) for complete usage.
 
 ## Installation
@@ -18,4 +21,3 @@ end
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at <https://hexdocs.pm/keex>.
-
--- mix.exs
+++ mix.exs
@@ -1,28 +1,51 @@
 defmodule Keex.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/dennym/keex"
+  @version "0.1.0"
+
   def project do
     [
       app: :keex,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13-rc",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      name: "Keex",
+      package: package(),
+      deps: deps(),
+      docs: docs()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.14", only: :test},
+    ]
+  end
+
+  def package do
+    [
+      description: "Simple Elixir wrapper for the Keen API",
+      maintainers: ["Denny Mueller"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  def docs do
+    [
+      extras: [
+        "LICENSE.md": [title: "License"],
+        "README.md": [title: "Overview"],
+        "guides/getting_started.md": [title: "Getting Started"]
+      ],
+      main: "readme",
+      source_url: @source_url,
+      formatters: ["html"]
     ]
   end
 end
